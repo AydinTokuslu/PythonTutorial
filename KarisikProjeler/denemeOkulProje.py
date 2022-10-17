56d58b140b3c1af97ebb592fa22736180e9fc2aa
Aydın Tokuşlu <aydintokuslu78@gmail.com>
2022-10-17 08:19:17 +0300

    17.10.22

--- KarisikProjeler/denemeOkulProje.py
+++ KarisikProjeler/denemeOkulProje.py
@@ -147,9 +147,6 @@ def sinif_listele(okul:dict):
     for i in okul:
         print("{}. sınıf".format(i))
 
-def sinif_listelelele(okul:dict):
-    for i in okul:
-        print("{}. sınıf".format(i))
 
 def sinif_ekle(okul:dict):
     print("Sadece 1., 2., 3. ve 4. sınıfları ekleyebilirsiniz.")
