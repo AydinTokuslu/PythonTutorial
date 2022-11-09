37271a75d554945c747ecde75c2bace67b520275
aydin <aydintokuslu78@gmail.com>
2022-11-09 20:26:36 +0300

    09.11.22

--- İlacReceteProje/İlacRecete.py
+++ İlacReceteProje/İlacRecete.py
@@ -6,6 +6,26 @@ import time
 #3- hastalığa ve ilaca bağlı bir reçete oluşturalım
 #4- reçeteleri ana listin içine atalım
 
+class Eczane():
+    def __init__(self):
+        self.hasta_adi = ""
+        self.hasta_soyadi = ""
+        self.hasta_sikayet = ""
+    def get_hastaAdi(self):
+        return self.hasta_adi
+    def get_hastaSoyadi(self):
+        return self.hasta_soyadi
+    def get_hastaSikayet(self):
+        return self.hasta_sikayet
+    def set_hastaAdi(self, hasta_adi):
+        self.hasta_adi = hasta_adi
+    def set_hastaSoyadi(self, hasta_soyadi):
+        self.hasta_soyadi = hasta_soyadi
+    def set_hastaSikayet(self, hasta_sikayet):
+        self.hasta_sikayet = hasta_sikayet
+
+
+
 hasta_list = []
 recete_list = {1 : "Parol",
                2 : "Aspirin",
@@ -46,9 +66,9 @@ doktor_listesi = {"Hakan" : "Allerji",
                   "Aslı" : "Genel Cerrahi",
                   "Asuman" : "Genel Cerrahi",
                   "Sare" : "Genel Cerrahi" }
-ilac_ödeme = 0
+ilac_odeme = 0
 
-def recete_kontrol(ilac_ödeme):
+def recete_kontrol(ilac_odeme):
     recete_k = True
     while recete_k:
         recete_no = int(input("Lütfen reçete numaranızı giriniz (1-8) : "))
@@ -61,13 +81,13 @@ def recete_kontrol(ilac_ödeme):
                 if recete_list[recete_no] in ilac_fiyatlari.keys():
                     print(
                         f"ilacınız olan {recete_list[recete_no]}'in fiyatı : {ilac_fiyatlari[recete_list[recete_no]]}'tldir")
-                ilac_ödeme += ilac_fiyatlari[recete_list[recete_no]]
-                print(f"Yapacağınız toplam ödeme {ilac_ödeme} tldir.")
+                ilac_odeme += ilac_fiyatlari[recete_list[recete_no]]
+                print(f"Yapacağınız toplam ödeme {ilac_odeme} tldir.")
                 sec = input("Ödeme yapmak için 1'i, başka bir ilaç almak için 2'yi tuşlayınız : ")
                 if sec == "1":
-                    odeme(ilac_ödeme)
+                    odeme(ilac_odeme)
                 elif sec == "2":
-                    recete_kontrol(ilac_ödeme)
+                    recete_kontrol(ilac_odeme)
                 else:
                     print("Yanlış tercihte bulundunuz. tekrar deneyiniz.")
             elif sec == "2":
@@ -86,9 +106,10 @@ def recete_kontrol(ilac_ödeme):
 
 
 def hasta_kayit():
+    hasta = Eczane()
     print("Giriş yapılan hasta kayıtlı değildir. Yeni kayıt yapılacaktır.")
-    hasta = input("lütfen hasta adı giriniz : ")
-    hasta_list.append(hasta)
+    hasta_adi = input("lütfen hasta adı giriniz : ")
+    hasta_list.append(hasta.hasta_adi)
     print("Hasta kaydı tamamlanmıştır.\nReçete ekranına yönlendiriliyorsunuz....")
     for i in range(3):
         time.sleep(1)
@@ -106,7 +127,7 @@ def ilacYazim():
     hastalik=input("Rahatsızlığınızı giriniz : ")
     for i in hastalikList:
         if hastalik == i:
-            recete_kontrol(ilac_ödeme)
+            recete_kontrol(ilac_odeme)
         elif hastalik != i:
             hastalikList.append(hastalik)
         else:
@@ -115,24 +136,31 @@ def ilacYazim():
 #global hasta_adi
 #global hasta_soyadi
 #global hasta_sikayet
-hasta_adi = ""
-hasta_soyadi = ""
-hasta_sikayet = ""
+# hasta_adi = ""
+# hasta_soyadi = ""
+# hasta_sikayet = ""
 
-def hasta_girisi(hasta_adi, hasta_soyadi, hasta_sikayet):
+
+
+def hasta_girisi():
+    hasta = Eczane()
     dosya = open("hasta_bilgileri.txt", "a", encoding="utf-8")
     hastalıkListesi()
-    hasta_adi = input("Lütfen hasta adını giriniz : ")
-    hasta_soyadi = input("Lütfen hasta soyadını giriniz : ")
-    hasta_sikayet = input("Lütfen yukarıdaki listeden hasta şikayetini giriniz : ")
+    #hasta_adi = hasta.set_hastaAdi(input("Lütfen hasta adını giriniz : "))
+    hasta.set_hastaAdi(input("Lütfen hasta adını giriniz : "))
+    #hasta_soyadi = hasta.set_hastaSoyadi(input("Lütfen hasta soyadını giriniz : "))
+    hasta.set_hastaSoyadi(input("Lütfen hasta soyadını giriniz : "))
+    #hasta_sikayet = hasta.set_hastaSikayet(input("Lütfen yukarıdaki listeden hasta şikayetini giriniz : "))
+    hasta.set_hastaSikayet(input("Lütfen yukarıdaki listeden hasta şikayetini giriniz : "))
     print("Kaydınız tamamlanmıştır. Ana menüye yönlendiriliyorsunuz...")
-    dosya.write(hasta_adi+"/"+hasta_soyadi+"/"+hasta_sikayet+"\n")
+    dosya.write(hasta.hasta_adi+"/"+hasta.hasta_soyadi+"/"+hasta.hasta_sikayet+"\n")
     main()
     dosya.close()
-    return hasta_adi, hasta_soyadi, hasta_sikayet
+    return hasta.get_hastaAdi(), hasta.get_hastaSoyadi(), hasta.get_hastaSikayet()
 
 
 def randevu_alma(hasta_adi, hasta_soyadi, hasta_sikayet):
+    hasta = Eczane()
     print("Görevli Doktor Listesi : ")
     print("S.No : Doktor İsmi :         Branşı : ")
     print("-------------------------------------")
@@ -142,27 +170,27 @@ def randevu_alma(hasta_adi, hasta_soyadi, hasta_sikayet):
         print("{:<3}-   Dr.{:<10} ------> {:<10}".format(a, i[0], i[1]))
     doktor = input("Lütfen muayene olmak istediğiniz doktor ismini DR ünvanı olmadan giriniz : ")
     if doktor in doktor_listesi.keys():
-        muayene_gun = input("Lütfen muayene olmak istediğiniz günü giriniz")
-        muayene_saati = input("Lütfen muayene olmak istediğiniz saati giriniz")
+        muayene_gun = input("Lütfen muayene olmak istediğiniz günü giriniz : ")
+        muayene_saati = input("Lütfen muayene olmak istediğiniz saati giriniz : ")
         print(f"Sayın {hasta_adi} {hasta_soyadi}, {hasta_sikayet} şikayetiyle yapmış olduğunuz muayeneniz Dr.{doktor} ile {muayene_gun} günü ve saat {muayene_saati}'de yapılacaktır.")
     else:
         print("Yanlış doktor ismi girdiniz. Tekrar deneyiniz.")
 
 
-def odeme(ilac_ödeme):
-    print(f"Ödemeniz gereken toplam tutar : {ilac_ödeme}")
+def odeme(ilac_odeme):
+    print(f"Ödemeniz gereken toplam tutar : {ilac_odeme}")
     sec = input("Ödemenizi kredi kartı ile ödemek için 1'i, havale ile ödemek için 2'yi tuşlayınız")
     if sec == "1":
         kart_no = input("Lütfen TR yazmadan ve boşluk bırakmadan 16 haneli kart numaranızı giriniz : ")
         if len(kart_no) != 16:
             print("Eksik veya fazla rakam girişi yaptınız. ")
             print("Lütfen TR yazmadan ve boşluk bırakmadan 16 haneli kart numaranızı giriniz : ")
-            odeme(ilac_ödeme)
+            odeme(ilac_odeme)
         elif len(kart_no) == 16:
             print("Kart üzerinde yazan Ad ve Soyad bilgilerini giriniz : ")
             print("Kart üzerinde yazan son kullanma tarihini giriniz : ")
             print("Kart üzerinde yazan güvenlik numarasını giriniz : ")
-            print(f"Ödemeniz gereken toplam tutar : {ilac_ödeme}")
+            print(f"Ödemeniz gereken toplam tutar : {ilac_odeme}")
             print("Ödemeniz başarı ile yapılmıştır. Teşekkür eder, yine bekleriz. Ana menüye yönlendiriliyorsunuz...")
             main()
     elif sec == "2":
@@ -172,7 +200,7 @@ def odeme(ilac_ödeme):
         main()
     else:
         print("Yanlış işlem girdiniz. İşleminizi tekrar ediniz. ")
-        odeme(ilac_ödeme)
+        odeme(ilac_odeme)
 
 
 def hasta_bilgilerini_goruntuleme():
@@ -191,15 +219,15 @@ def main():
         if secim == "1":
             hasta_bilgilerini_goruntuleme()
         elif secim == "2":
-            recete_kontrol(ilac_ödeme)
+            recete_kontrol(ilac_odeme)
         elif secim == "3":
-            randevu_alma(hasta_adi, hasta_soyadi, hasta_sikayet)
+            randevu_alma()
         elif secim == "4":
-            odeme(ilac_ödeme)
+            odeme(ilac_odeme)
         elif secim == "5":
             quit()
         else:
             print("Yanlış tercihte bulundunuz. Tekrar deneyiniz.")
 
-hasta_girisi(hasta_adi, hasta_soyadi, hasta_sikayet)
+hasta_girisi()
 
