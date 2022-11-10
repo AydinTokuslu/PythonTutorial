2693a9d87d0201a0e539379f5f2f7780537c1775
aydin <aydintokuslu78@gmail.com>
2022-11-10 21:06:22 +0300

    10.11.22

--- class_olusturma_ornekleri/class_iskambil_destesi.py
+++ class_olusturma_ornekleri/class_iskambil_destesi.py
@@ -1,3 +1,5 @@
+import random
+
 class Card(object):
     def __init__(self, suit, value):
         self.suit= suit
@@ -6,11 +8,44 @@ class Card(object):
         print("{} of {}".format(self.value, self.suit))
     pass
 class Deck(object):  # kartlar
-    pass
+    def __init__(self):
+        self.cards = []
+        self.build()
+        self.shuffle()
+        #self.show_deck()
+    def build(self):
+        for s in ["Spades", "Diamonds", "Hearts", "Clubs"]:  # Maça, Karo, Kupa, Sinek
+            for v in range(1,14):
+                self.cards.append(Card(s,v))
+    def show_deck(self):
+        for card in self.cards:
+            card.show_card()
+    def shuffle(self):
+        for i in range(len(self.cards)-1, 0, -1):
+            r = random.randint(0,i)
+            self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+    def draw_card(self):
+        return self.cards.pop()
+
 class Player(object):
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.hand =[]
+    def draw(self,deck):
+        self.hand.append(deck.draw_card())
+        return self
+    def show_hand(self):
+        for card in self.hand:
+            card.show_card()
+    def discard(self):
+        return self.hand.pop()
 
 def main():
-    card = Card("Hearts", 10)  # Hearts = Kupa
+    deck = Deck()
+    deck.shuffle()
+    Ali = Player("Ali")
+    Ali.draw(deck).draw(deck)
+    Ali.show_hand()
+
 
 main()
