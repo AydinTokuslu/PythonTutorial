213db52daee3758cf88a13e009d0270d12622ea0
aydin <aydintokuslu78@gmail.com>
2022-11-15 22:20:29 +0300

    15.11.22

---
+++ Ders_Konulari/for_loop.py
@@ -0,0 +1,14 @@
+# Task - 5'ler tablosu
+
+# sayi=5
+# for i in range(11):
+#     print(f"{sayi} x {i} = {sayi * i}")
+
+# number = int(input("enter a number between 1 and 10: "))
+# for i in range(11):
+#     print(f"{number,i}x{number * i}=")
+#     #i += 1
+
+print(range(5))
+print(*range(5))  # * boşluklu yazdırıyor, 0 1 2 3 4
+print(*("seperate")) # * boşluklu yazdırıyor, s e p e r a t e
