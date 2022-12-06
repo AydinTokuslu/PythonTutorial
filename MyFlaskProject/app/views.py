d5f40baa5578fc90a04c82c3108c3684fe49ab38
aydin <aydintokuslu78@gmail.com>
2022-12-06 14:06:36 +0300

    06.12.22

--- MyFlaskProject/app/views.py
+++ MyFlaskProject/app/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 # web sayfasına erişim, bilgileri okuma projesi
 app = Flask(__name__)
@@ -13,3 +13,69 @@ def Definition():   # herhangi bir fonksiyon ismi veriyoruz.
 @app.route("/hello")
 def Hello():
     return render_template("hello.html")
+
+@app.route("/hello-admin")
+def HelloAdmin():
+    return render_template("hello_admin.html")
+
+#https status code belirtilmemişse GET'tir, belirtilmişse POST'tur
+@app.route("/hello-user/<name>")
+def HelloUser(name):
+    if name.lower() == "admin":
+        return redirect(url_for("HelloAdmin"))
+    return render_template("hello_user.html", username = name)
+
+
+# @app.route("/add/<int:number1>/<int:number2>")
+# def Add(number1, number2):#burda number1 number2 string, int yapmak gerekir.
+#     calculation_result = number1 + number2
+#     return render_template("add.html", number1 = number1, number2=number2, result=calculation_result)
+# aşağıdaki farklı bir çözümü
+
+@app.route("/add") # yazarken /add?number1=12&number2=13 olarak yazıyoruz ekrana
+def Add():
+    number1 = int(request.args["number1"])
+    number2 = int(request.args["number2"])
+    calculation_result = number1 + number2
+    return render_template("add.html", number1 = number1, number2=number2, result=calculation_result)
+
+
+@app.route("/login", methods=['POST','GET'])
+def Login():
+    if request.method == "POST":
+        username = request.form["username"]
+        return redirect(url_for("HelloUser", name=username))
+    else:
+        return render_template("login.html")
+
+@app.route("/student")
+def Student():
+    return render_template("student.html")
+
+@app.route("/result", methods=['POST'])
+def Result():
+    # ikinci yol - kısa olan
+    ContextData = {
+        "name" : request.form["name"],
+        "physics" : request.form["physics"],
+        "mathematics" : request.form["mathematics"],
+        "chemistry" : request.form["chemistry"]
+    }
+    return render_template("student_result.html", **ContextData)
+    # birinci yol - uzun olan
+    # name = request.form["name"]
+    # physics = request.form["physics"]
+    # mathematics = request.form["mathematics"]
+    # chemistry = request.form["chemistry"]
+    # return render_template("student_result.html", name=name,
+    #                        physics=physics,
+    #                        mathematics=mathematics,
+    #                        chemistry=chemistry)
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
