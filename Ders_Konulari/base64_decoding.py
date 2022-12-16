9e1a351af9b4ba46dc272a8b3b35650467a2fb84
aydin <aydintokuslu78@gmail.com>
2022-12-16 08:38:25 +0300

    16.12.22

--- Ders_Konulari/base64_decoding.py
+++ Ders_Konulari/base64_decoding.py
@@ -10,3 +10,17 @@ print(string_as_binary)  #bunu stringe çevirmemiz gerekir, şu an bytearray
 
 string = string_as_binary.decode("utf-8")
 print(string)  # Python is awesome
+
+
+##########################
+# sayıları decoding yapma
+
+number_as_base64 = b'cyTMWZzG'
+# number = 218374823748723, aradığımız sayı
+
+number_as_bytes = base64.b64decode(number_as_base64)
+print("Sayımızın byte'ları : ", number_as_bytes)
+# Sayımızın byte'ları :  b's$\xccY\x9c\xc6'
+
+number = int.from_bytes(number_as_bytes, byteorder="little")
+print(number)  # 218374823748723
