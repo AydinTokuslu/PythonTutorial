c3da2283b52912397feeb19b3959b3a40b33e8c3
aydin <aydintokuslu78@gmail.com>
2022-12-22 20:40:09 +0300

    22.12.22

--- 50_days_of_python/day11.py
+++ 50_days_of_python/day11.py
@@ -3,38 +3,27 @@
 # (if they have the same characters and have equal length), it
 # should return True, if they are not, it should return False. For
 # example, ‘love’ and ‘evol’ should return True.
-import random
-from random import choice, choices
+
 def equal_strings(str1,str2):
-    ortak_eleman = ""
-    #global ortak_eleman
+    farkli_eleman = ""
     global random_list
     random_list = []
     if len(str1) == len(str2):
         print("iki kelimenin uzunluğu aynıdır")
         for i in range(len(str1)):
-            if str1[i] in str2:
-                print("aynı harflere sahiplerdir.")
-                ortak_eleman += str1[i]
-                #match(ortak_eleman, str2)
-            elif str2[i] in str1:
-                print("aynı harflere sahiplerdir.")
-            else:
-                print("aynı harflere sahip değillerdir.")
-                return False
-        # print("ortak_elemanlar : ", ortak_eleman)
-
+            if str1[i] not in str2:
+                farkli_eleman += str1[i]
+            elif str2[i] not in str1:
+                farkli_eleman += str2[i]
     else:
         print("iki kelimenin uzunluğu aynı değildir")
         return False
-    #print("ortak_elemanlar : ", ortak_eleman)
-# def match(ortak_eleman, str2):
-#     random_list.append(random.choices(ortak_eleman, k=len(ortak_eleman)))
-#     print(random_list)
-#     if ortak_eleman == str2:
-#         print("iki kelime aynı harflere sahiptir.")
-#         return True
-#     else:
-#         print("iki kelime aynı harflere sahip değildir.")
 
-print(equal_strings("love", "eval"))
+    if len(farkli_eleman) == 0:
+        print("iki kelime aynı harflere sahiptir.")
+        return True
+    else:
+        print("iki kelime aynı harflere sahip değildir.")
+        return False
+
+print(equal_strings("love", "evol"))
