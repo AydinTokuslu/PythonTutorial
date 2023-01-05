af1a9da7165ec6f468cee7d0c8bb4d68d06af419
aydin <aydintokuslu78@gmail.com>
2023-01-05 14:49:24 +0300

    05.01.23

--- 50_days_of_python/day17_extra.py
+++ 50_days_of_python/day17_extra.py
@@ -7,11 +7,9 @@
 # Do not use the built-in sort functions
 
 def sort_length(list):
-    new_list=[]
-    max=0
     for i in range(len(list)):
         for j in range(i + 1, len(list)):
-            if list[i] > list[j]:
+            if len(list[i]) > len(list[j]):
                 list[i], list[j] = list[j], list[i]
     return list
 
