0c5816548eb06833daca163db3dc9b58b06b2e26
aydin <aydintokuslu78@gmail.com>
2023-01-11 00:07:06 +0300

    11.01.23

--- 50_days_of_python/day22.py
+++ 50_days_of_python/day22.py
@@ -8,32 +8,41 @@
 # print(remove_underscore(add_underscore(add_hash('Python'))))
 # it should return ‘Python’.
 
-def add_hash(str):
-    new_str=""
-    for i in str:
-        new_str+=i
-        new_str+="#"
-    return new_str
+# def add_hash(str):
+#     new_str=""
+#     for i in str:
+#         new_str+=i
+#         new_str+="#"
+#     return new_str
+#
+# def add_underscore(str):
+#     new_str=""
+#     for i in str:
+#         if i == "#":
+#             continue
+#         else:
+#             new_str+=i
+#             new_str+="_"
+#     return new_str
+#
+# def remove_underscore(str):
+#     new_str = ""
+#     for i in str:
+#         if i == "_":
+#             continue
+#         else:
+#             new_str += i
+#     return new_str
+#
+# print(remove_underscore(add_underscore(add_hash("Python"))))
 
-def add_underscore(str):
-    new_str=""
-    for i in str:
-        if i == "#":
-            continue
-        else:
-            new_str+=i
-            new_str+="_"
-    return new_str
+def add_hash(a: str):
+ return "#".join(a)
+def add_underscore(a: str):
+ return str(a).replace("#", "_")
+def remove_underscore(a: str):
+ return str(a).replace("_", "")
+print(remove_underscore(add_underscore(add_hash('Python'))))
 
-def remove_underscore(str):
-    new_str = ""
-    for i in str:
-        if i == "_":
-            continue
-        else:
-            new_str += i
-    return new_str
-
-print(remove_underscore(add_underscore(add_hash("Python"))))
 
 
